eecf1962db8a778000806143ddaa418769b04829
davidhalldor <davidhalldor@0bb18136-d148-0410-99f0-b708941b1824>
2008-03-24 14:01:30 +0000

    Útfærði útlitið hans Óla í verkefnið

--- meukow/meukow/MainWindow.Designer.cs
+++ meukow/meukow/MainWindow.Designer.cs
@@ -33,12 +33,29 @@
 			this.listBindingSource = new System.Windows.Forms.BindingSource(this.components);
 			this.m_mainMenu = new System.Windows.Forms.MenuStrip();
 			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.m_toolStrip = new System.Windows.Forms.ToolStrip();
-			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.m_tabVinsaeldarlistar = new System.Windows.Forms.TabPage();
+			this.m_toolStripVinlistar = new System.Windows.Forms.ToolStrip();
 			this.hitParadeView1 = new meukow.HitParadeView();
+			this.m_tabLog = new System.Windows.Forms.TabPage();
+			this.m_toolStripLog = new System.Windows.Forms.ToolStrip();
+			this.m_tSbtnSkodalog = new System.Windows.Forms.ToolStripButton();
+			this.m_tSbtnSkralog = new System.Windows.Forms.ToolStripButton();
+			this.m_tabFlytjendur = new System.Windows.Forms.TabPage();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.m_tSbtnSkodaflytjendur = new System.Windows.Forms.ToolStripButton();
+			this.m_tSbtnSkraflytjendur = new System.Windows.Forms.ToolStripButton();
+			this.m_tSbtnSkodavinlistar = new System.Windows.Forms.ToolStripButton();
+			this.m_tSbtnSkravinlista = new System.Windows.Forms.ToolStripButton();
 			((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).BeginInit();
 			this.m_mainMenu.SuspendLayout();
-			this.m_toolStrip.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.m_tabVinsaeldarlistar.SuspendLayout();
+			this.m_toolStripVinlistar.SuspendLayout();
+			this.m_tabLog.SuspendLayout();
+			this.m_toolStripLog.SuspendLayout();
+			this.m_tabFlytjendur.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// listBindingSource
@@ -61,40 +78,152 @@
 			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
 			this.fileToolStripMenuItem.Text = "File";
 			// 
-			// m_toolStrip
+			// tabControl1
 			// 
-			this.m_toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1});
-			this.m_toolStrip.Location = new System.Drawing.Point(0, 24);
-			this.m_toolStrip.Name = "m_toolStrip";
-			this.m_toolStrip.Size = new System.Drawing.Size(631, 25);
-			this.m_toolStrip.TabIndex = 1;
-			this.m_toolStrip.Text = "toolStrip1";
+			this.tabControl1.Controls.Add(this.m_tabVinsaeldarlistar);
+			this.tabControl1.Controls.Add(this.m_tabLog);
+			this.tabControl1.Controls.Add(this.m_tabFlytjendur);
+			this.tabControl1.Location = new System.Drawing.Point(0, 24);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(631, 539);
+			this.tabControl1.TabIndex = 3;
 			// 
-			// toolStripButton1
+			// m_tabVinsaeldarlistar
 			// 
-			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton1.Name = "toolStripButton1";
-			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton1.Text = "toolStripButton1";
+			this.m_tabVinsaeldarlistar.Controls.Add(this.m_toolStripVinlistar);
+			this.m_tabVinsaeldarlistar.Controls.Add(this.hitParadeView1);
+			this.m_tabVinsaeldarlistar.Location = new System.Drawing.Point(4, 22);
+			this.m_tabVinsaeldarlistar.Name = "m_tabVinsaeldarlistar";
+			this.m_tabVinsaeldarlistar.Padding = new System.Windows.Forms.Padding(3);
+			this.m_tabVinsaeldarlistar.Size = new System.Drawing.Size(623, 513);
+			this.m_tabVinsaeldarlistar.TabIndex = 0;
+			this.m_tabVinsaeldarlistar.Text = "Vinsældarlistar";
+			this.m_tabVinsaeldarlistar.UseVisualStyleBackColor = true;
+			// 
+			// m_toolStripVinlistar
+			// 
+			this.m_toolStripVinlistar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_tSbtnSkodavinlistar,
+            this.m_tSbtnSkravinlista});
+			this.m_toolStripVinlistar.Location = new System.Drawing.Point(3, 3);
+			this.m_toolStripVinlistar.Name = "m_toolStripVinlistar";
+			this.m_toolStripVinlistar.Size = new System.Drawing.Size(617, 25);
+			this.m_toolStripVinlistar.TabIndex = 3;
+			this.m_toolStripVinlistar.Text = "toolStrip2";
 			// 
 			// hitParadeView1
 			// 
 			this.hitParadeView1.Document = null;
-			this.hitParadeView1.Location = new System.Drawing.Point(13, 53);
+			this.hitParadeView1.Location = new System.Drawing.Point(0, 31);
 			this.hitParadeView1.Name = "hitParadeView1";
-			this.hitParadeView1.Size = new System.Drawing.Size(415, 435);
+			this.hitParadeView1.Size = new System.Drawing.Size(415, 424);
 			this.hitParadeView1.TabIndex = 2;
+			// 
+			// m_tabLog
+			// 
+			this.m_tabLog.Controls.Add(this.m_toolStripLog);
+			this.m_tabLog.Location = new System.Drawing.Point(4, 22);
+			this.m_tabLog.Name = "m_tabLog";
+			this.m_tabLog.Padding = new System.Windows.Forms.Padding(3);
+			this.m_tabLog.Size = new System.Drawing.Size(623, 513);
+			this.m_tabLog.TabIndex = 1;
+			this.m_tabLog.Text = "Lög";
+			this.m_tabLog.UseVisualStyleBackColor = true;
+			// 
+			// m_toolStripLog
+			// 
+			this.m_toolStripLog.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_tSbtnSkodalog,
+            this.m_tSbtnSkralog});
+			this.m_toolStripLog.Location = new System.Drawing.Point(3, 3);
+			this.m_toolStripLog.Name = "m_toolStripLog";
+			this.m_toolStripLog.Size = new System.Drawing.Size(617, 25);
+			this.m_toolStripLog.TabIndex = 0;
+			this.m_toolStripLog.Text = "toolStrip1";
+			// 
+			// m_tSbtnSkodalog
+			// 
+			this.m_tSbtnSkodalog.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkodalog.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkodalog.Image")));
+			this.m_tSbtnSkodalog.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkodalog.Name = "m_tSbtnSkodalog";
+			this.m_tSbtnSkodalog.Size = new System.Drawing.Size(57, 22);
+			this.m_tSbtnSkodalog.Text = "Skoða lög";
+			// 
+			// m_tSbtnSkralog
+			// 
+			this.m_tSbtnSkralog.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkralog.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkralog.Image")));
+			this.m_tSbtnSkralog.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkralog.Name = "m_tSbtnSkralog";
+			this.m_tSbtnSkralog.Size = new System.Drawing.Size(49, 22);
+			this.m_tSbtnSkralog.Text = "Skrá lög";
+			// 
+			// m_tabFlytjendur
+			// 
+			this.m_tabFlytjendur.Controls.Add(this.toolStrip1);
+			this.m_tabFlytjendur.Location = new System.Drawing.Point(4, 22);
+			this.m_tabFlytjendur.Name = "m_tabFlytjendur";
+			this.m_tabFlytjendur.Padding = new System.Windows.Forms.Padding(3);
+			this.m_tabFlytjendur.Size = new System.Drawing.Size(623, 513);
+			this.m_tabFlytjendur.TabIndex = 2;
+			this.m_tabFlytjendur.Text = "Flytjendur";
+			this.m_tabFlytjendur.UseVisualStyleBackColor = true;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_tSbtnSkodaflytjendur,
+            this.m_tSbtnSkraflytjendur});
+			this.toolStrip1.Location = new System.Drawing.Point(3, 3);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(617, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// m_tSbtnSkodaflytjendur
+			// 
+			this.m_tSbtnSkodaflytjendur.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkodaflytjendur.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkodaflytjendur.Image")));
+			this.m_tSbtnSkodaflytjendur.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkodaflytjendur.Name = "m_tSbtnSkodaflytjendur";
+			this.m_tSbtnSkodaflytjendur.Size = new System.Drawing.Size(90, 22);
+			this.m_tSbtnSkodaflytjendur.Text = "Skoða flytjendur";
+			// 
+			// m_tSbtnSkraflytjendur
+			// 
+			this.m_tSbtnSkraflytjendur.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkraflytjendur.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkraflytjendur.Image")));
+			this.m_tSbtnSkraflytjendur.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkraflytjendur.Name = "m_tSbtnSkraflytjendur";
+			this.m_tSbtnSkraflytjendur.Size = new System.Drawing.Size(82, 22);
+			this.m_tSbtnSkraflytjendur.Text = "Skrá flytjendur";
+			// 
+			// m_tSbtnSkodavinlistar
+			// 
+			this.m_tSbtnSkodavinlistar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkodavinlistar.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkodavinlistar.Image")));
+			this.m_tSbtnSkodavinlistar.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkodavinlistar.Name = "m_tSbtnSkodavinlistar";
+			this.m_tSbtnSkodavinlistar.Size = new System.Drawing.Size(109, 22);
+			this.m_tSbtnSkodavinlistar.Text = "Skoða vinsældarlista";
+			// 
+			// m_tSbtnSkravinlista
+			// 
+			this.m_tSbtnSkravinlista.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.m_tSbtnSkravinlista.Image = ((System.Drawing.Image)(resources.GetObject("m_tSbtnSkravinlista.Image")));
+			this.m_tSbtnSkravinlista.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.m_tSbtnSkravinlista.Name = "m_tSbtnSkravinlista";
+			this.m_tSbtnSkravinlista.Size = new System.Drawing.Size(101, 22);
+			this.m_tSbtnSkravinlista.Text = "Skrá vinsældarlista";
 			// 
 			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(631, 575);
-			this.Controls.Add(this.hitParadeView1);
-			this.Controls.Add(this.m_toolStrip);
+			this.Controls.Add(this.tabControl1);
 			this.Controls.Add(this.m_mainMenu);
 			this.MainMenuStrip = this.m_mainMenu;
 			this.Name = "MainWindow";
@@ -102,8 +231,19 @@
 			((System.ComponentModel.ISupportInitialize)(this.listBindingSource)).EndInit();
 			this.m_mainMenu.ResumeLayout(false);
 			this.m_mainMenu.PerformLayout();
-			this.m_toolStrip.ResumeLayout(false);
-			this.m_toolStrip.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.m_tabVinsaeldarlistar.ResumeLayout(false);
+			this.m_tabVinsaeldarlistar.PerformLayout();
+			this.m_toolStripVinlistar.ResumeLayout(false);
+			this.m_toolStripVinlistar.PerformLayout();
+			this.m_tabLog.ResumeLayout(false);
+			this.m_tabLog.PerformLayout();
+			this.m_toolStripLog.ResumeLayout(false);
+			this.m_toolStripLog.PerformLayout();
+			this.m_tabFlytjendur.ResumeLayout(false);
+			this.m_tabFlytjendur.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -114,9 +254,20 @@
 		private System.Windows.Forms.BindingSource listBindingSource;
 		private System.Windows.Forms.MenuStrip m_mainMenu;
 		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-		private System.Windows.Forms.ToolStrip m_toolStrip;
-		private System.Windows.Forms.ToolStripButton toolStripButton1;
 		private HitParadeView hitParadeView1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage m_tabVinsaeldarlistar;
+		private System.Windows.Forms.TabPage m_tabLog;
+		private System.Windows.Forms.TabPage m_tabFlytjendur;
+		private System.Windows.Forms.ToolStrip m_toolStripLog;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkodalog;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkralog;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkodaflytjendur;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkraflytjendur;
+		private System.Windows.Forms.ToolStrip m_toolStripVinlistar;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkodavinlistar;
+		private System.Windows.Forms.ToolStripButton m_tSbtnSkravinlista;
 	}
 }
 
